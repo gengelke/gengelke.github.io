c0149cea18397aa95779571dbe82c91d5d76ca4d
Gordon Engelke <ngg8fe@bosch.com>
2020-02-08 17:39:03 +0100

    initial version

---
+++ _site/README.md
@@ -0,0 +1,25 @@
+# Gordon Engelke's personal blog
+
+This is Gordon Engelke's personal blog. It is built with the [minimal
+mistakes](https://mmistakes.github.io/minimal-mistakes/) jekyll theme.
+
+## To run
+
+```ruby
+rake build
+```
+
+Because of a small unicode error/bug, see [this blog
+post](https://www.janmeppe.com/blog/invalid-US-ASCII-character/) we build the
+site using `rake build` instead of `bundle exec jekyll serve/build`
+
+## To view draft posts
+
+(This is included in the rakefile.) To preview draft posts, put **undated**
+posts in the `_drafts/` folder and then build/serve jekyll with the `--draft`
+flag.
+
+## Links
+
+* [View my blog here](https://gengelke.github.io/)
+* [View configuration options here](https://mmistakes.github.io/minimal-mistakes/docs/configuration/).
---
+++ _site/assets/js/lunr/lunr-store.js
@@ -0,0 +1,36 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "http://localhost:4000/blog/welcome-to-jekyll/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "How to change Jekyll theme to Minimal Mistakes",
+        "excerpt":"Recently I migrated my whole blog from the Cayman theme to the Minimal Mistakes theme, which can be found here. This blog post explains how to quickly get up and running with the Minimal Mistakes theme. Before After Setting this is up is ridiculously simple if you know what you’re...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "http://localhost:4000/blog/how-to-change-theme-to-minimal-mistakes/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "Invalid US-ASCII character \"\\xE2\" on line 54 workaround",
+        "excerpt":"In this blog post I cover the infamous “Invalid US-ASCII character \"\\xE2\" on line 54” error that seems to plague people when trying to serve their sites locally. OK, I actually don’t fix the problem but I create a workaround. The problem Have you ever ran into this issue? Error:...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "http://localhost:4000/blog/invalid-US-ASCII-character/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "How to add Latex to Minimal Mistakes",
+        "excerpt":"In one of my previous blog posts I show you how to set up the Minimal Mistakes theme for your Github page. In this blog post I show how to add Latex support to your Minimal Mistakes jekyll blog and is largely based on this. Step 1. Set markdown engine...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "http://localhost:4000/blog/How-to-add-mathjax-to-minimal-mistakes/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "The Maker vs Manager Problem",
+        "excerpt":"It’s well known that programmers dislike meetings… a lot. But have you ever wondered why programmers dislike meetings so much compared to other people? The answer is simple but obvious once pointed out: meetings are more costly to them. I’m sure many programmers can relate to this comic. This blog...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "http://localhost:4000/blog/makers-vs-manager/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "Regex For Noobs (like me!) - An Illustrated Guide",
+        "excerpt":"This blog post is an illustrated guide to regex and aims to provide a gentle introduction for people who never have fiddled with regex, want to, but are kind of intimidated by the whole thing. In other words, welcome to … For most people without a formal CS education, regular...","categories": ["blog"],
+        "tags": ["Jekyll","update"],
+        "url": "http://localhost:4000/blog/regex-for-noobs/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"},{
+        "title": "Plotting With Matplotlib",
+        "excerpt":"Plotting with matplotlib is really confusing. Understanding how it works under the hood helps to alleviate this pain enormously. This short blog post is here to help you on the way to matplotlib enlightenment. Let’s jump right in! &gt;&gt;&gt; import matplotlib.pyplot as plt Key takeaways It is useful to understand...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/Plotting-with-Matplotlib/",
+        "teaser":"http://localhost:4000/assets/images/500x300.jpg"}]
